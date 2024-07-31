bf676bd821cae6e9c93dc865df3ef1172b339673
Icenowy Zheng <uwu@icenowy.me>
2024-07-31 03:52:40 +0000

    ttm: disallow cached mapping
    Signed-off-by: Icenowy Zheng <uwu@icenowy.me>
    
    Conflicts:
    	drivers/gpu/drm/ttm/ttm_tt.c

--- drivers/gpu/drm/ttm/ttm_tt.c
+++ drivers/gpu/drm/ttm/ttm_tt.c
@@ -159,7 +159,11 @@ static void ttm_tt_init_fields(struct ttm_tt *ttm,
 	ttm->dma_address = NULL;
 	ttm->swap_storage = NULL;
 	ttm->sg = bo->sg;
+#ifdef CONFIG_SOC_SIFIVE_EIC7700
+	ttm->caching = ttm_write_combined;
+#else
 	ttm->caching = caching;
+#endif
 	ttm->restore = NULL;
 	ttm->backup = NULL;
 }
